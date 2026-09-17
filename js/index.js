99a98c189dfbfb936e2534de9dd4f12f6fc206d9
mazellan <magellan4746@gmail.com>
2026-09-17 18:10:08 +0900

    에니메이션 수정

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sections = document.querySelectorAll(
-        '.con1-profile, .con-about-me-wrap, .con1-about-me, .con1-text-main, .text-contain-right, .con1-card-wrap, .con1-skill-card, .con2-popup, .con2-poster, .con2-banner, .con2-product-page'
+        '.l-title, .con1-profile, .con-about-me-wrap,.con1-profile-wrap, .con1-text-main, .text-contain-left, .text-contain-right, .con1-card-wrap, .con1-skill-card, .con2-popup, .con2-poster, .con2-banner, .con2-product-page'
     );
 
     // 각 작업 블록이 화면에 들어오면 왼쪽에서 나타나고, 벗어나면 다시 왼쪽으로 사라집니다.
